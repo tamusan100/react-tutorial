4458e42f9727250456afb966315fbdeaee2a2971
tamusan100 <tamusan100@gmail.com>
2022-03-19 11:25:35 +0900

    bulma適用

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import { render } from "react-dom"
+import "bulma/css/bulma.css";
+import { render } from "react-dom";
 import App from "./App";
 
 render(<App />, document.querySelector("#content"));
